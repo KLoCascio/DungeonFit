82380e85a73ef34c30f1c5683eca50ee310806f0
BWhittaker1415 <bwhittaker1415@gmail.com>
2023-11-08 09:24:07 -0500

    login and signUp page

--- DungeonFit/src/components/Login.jsx
+++ DungeonFit/src/components/Login.jsx
@@ -2,21 +2,59 @@
 
 import { useEffect, useState } from "react"
 import { useParams, Link } from 'react-router-dom'
+import loginLogo from '../assets/logo/Logo/signInLogo.svg'
 
 import axios from 'axios'
 
-export default function Login() {
+const Login = () => {
+    const login = {
+        username: '',
+        password: '',
+        // valid: 'Password must match'
+    }
+
+    const [formState, setFormState] = useState(login)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(formState)
+        // setFormState(login)
+    }
+
+    const handleChange = e => {
+        const { id, value } = e.target
+        setFormState({...formState, [id]: value})
+    }
+
     return (
         <div className="Login">
-            <img src="#" alt="Login Logo"></img>
+            <img src={loginLogo} alt="Logo"></img>
             <h1> Sign In</h1>
-            <form className="login-form">
-                <label htmlFor="login-userName">Username: </label>
-                <input type="text" className="login-userName" value="Enter Username..." />
-                <label htmlFor="login-password">Password: </label>
-                <input type="text" className="login-password" value="Enter Password..." />
+            <form onSubmit={handleSubmit} className="login-form">
+
+            <label htmlFor="userLogin">Username: </label>
+            <input 
+            type="text" 
+            id="login"
+            className="signup-userName" 
+            placeholder="Enter username..." 
+            value={formState.username} 
+            onChange={handleChange}
+            />
+
+            <label htmlFor="loginPassword">Password: </label>
+            <input 
+            type="password" 
+            id="loginPassword"
+            className="signup-password1" 
+            placeholder="Enter password..." 
+            onChange={handleChange} 
+            value={formState.password} 
+            />
+
+            <button type="submit" className="sign-in-button">Sign In</button>
+            {/* <p>{formState.valid}</p> */}
             </form>
-            <button className="sign-in-button">Sign In</button>
             <div className="Sign-Up">
                 <p>Don't have an account? Create one today!</p>
                 <button className="create-account-button"><Link to="/signup">Create Account</Link></button>
@@ -25,3 +63,5 @@ export default function Login() {
     )
 
 }
+
+export default Login
