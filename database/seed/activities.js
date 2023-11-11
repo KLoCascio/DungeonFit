90a9f65b18b7ac0995f9e8f0d46786d7cb6ee221
KLoCascio <KyndalLife@gmail.com>
2023-11-11 18:28:41 -0500

    activities functional, need clickable cards still

--- database/seed/activities.js
+++ database/seed/activities.js
@@ -5,64 +5,64 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 const main = async () => {
 
-   const activitiesData = [
-      {
-        activityDay: "Sunday",
-        activityIcon: "../DungeonFit/src/assets/icons/CardioIcon.png",
-        activityTitle: "Jogging",
-        activityType: "Cardio"
-      },
-      {
-        activityDay: "Monday",
-        activityIcon: "DungeonFit/src/assets/icons/LiftIcon.png",
-        activityTitle: "Chest Push",
-        activityType: "Upper Body"
-      },
-      {
-        activityDay: "Tuesday",
-        activityIcon: "../DungeonFit/src/assets/icons/RestIcon.png",
-        activityTitle: "Rest",
-        activityType: "Rest Day"
-      },
-      {
-        activityDay: "Wednesday",
-        activityIcon: "DungeonFit/src/assets/icons/LiftIcon.png",
-        activityTitle: "Lats and Triceps",
-        activityType: "Upper Body"
-      },
-      {
-        activityDay: "Thursday",
-        activityIcon: "DungeonFit/src/assets/icons/LiftIcon.png",
-        activityTitle: "Back Pull",
-        activityType: "Upper Body"
-      },
-      {
-        activityDay: "Friday",
-        activityIcon: "DungeonFit/src/assets/icons/LiftIcon.png",
-        activityTitle: "Leg Push",
-        activityType: "Lower Body"
-      },
-      {
-        activityDay: "Saturday",
-        activityIcon: "DungeonFit/src/assets/icons/LiftIcon.png",
-        activityTitle: "Chest and Biceps",
-        activityType: "Upper Body"
-      },
-      {
-        activityDay: "Sunday",
-        activityIcon: "../DungeonFit/src/assets/icons/RestIcon.png",
-        activityTitle: "Rest",
-        activityType: "Rest Day"
-      },
-   ]
+  const activitiesData = [
+    {
+      activityDay: "Sunday",
+      activityIcon: "./src/assets/icons/CardioIcon.png",
+      activityTitle: "Jogging",
+      activityType: "Cardio"
+    },
+    {
+      activityDay: "Monday",
+      activityIcon: "./src/assets/icons/LiftIcon.png",
+      activityTitle: "Chest Push",
+      activityType: "Upper Body"
+    },
+    {
+      activityDay: "Tuesday",
+      activityIcon: "./src/assets/icons/RestIcon.png",
+      activityTitle: "Rest",
+      activityType: "Rest Day"
+    },
+    {
+      activityDay: "Wednesday",
+      activityIcon: "./src/assets/icons/LiftIcon.png",
+      activityTitle: "Lats and Triceps",
+      activityType: "Upper Body"
+    },
+    {
+      activityDay: "Thursday",
+      activityIcon: "./src/assets/icons/LiftIcon.png",
+      activityTitle: "Back Pull",
+      activityType: "Upper Body"
+    },
+    {
+      activityDay: "Friday",
+      activityIcon: "./src/assets/icons/LiftIcon.png",
+      activityTitle: "Leg Push",
+      activityType: "Lower Body"
+    },
+    {
+      activityDay: "Saturday",
+      activityIcon: "./src/assets/icons/LiftIcon.png",
+      activityTitle: "Chest and Biceps",
+      activityType: "Upper Body"
+    },
+    {
+      activityDay: "Sunday",
+      activityIcon: "./src/assets/icons/RestIcon.png",
+      activityTitle: "Rest",
+      activityType: "Rest Day"
+    },
+  ]
 
-   await Activities.insertMany(activitiesData)
-   console.log('Created Activities')
+  await Activities.insertMany(activitiesData)
+  console.log('Created Activities')
 }
 
 const run = async () => {
-   await main()
-   db.close()
+  await main()
+  db.close()
 }
 
 run()
