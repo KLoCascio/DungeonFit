9b508c8d090cf25892aa97027114d37ab9487004
KLoCascio <KyndalLife@gmail.com>
2023-11-11 18:29:01 -0500

    back end re-seeded

---
+++ DungeonFit/src/details/ActivityDetails.jsx
@@ -0,0 +1,32 @@
+// Clickable Cards
+import React, { useState, useEffect } from 'react'
+import { useParams, Link } from 'react-router-dom'
+import axios from 'axios'
+
+export default function ActivityDetails() {
+    const [activity, setActivity] = useState(null)
+    const { id } = useParams()
+
+    useEffect(() => {
+        const getActivityDetails = async () => {
+            try {
+                const response = await axios.get(`http://localhost:3001/${id}`)
+                console.log("Response:", response.data)
+                setActivity(response.data)
+            } catch (e) {
+                console.error("Error Fetching Activity Details:", e)
+            }
+        }
+        getActivityDetails()
+    }, [id])
+
+    return activity ? (
+        <div className="activity-details">
+            <img className="details-icon" src={activity.activityIcon} alt="Activity Icon" />
+                  <h2 className="activity-title">{activity.activityTitle}</h2>
+                  <h3 className="activity-day">{activity.activityDay}</h3>
+                  <Link to="/activities" className="return-link">Return</Link>
+        </div>
+    ) : <h2 className="Loading">Loading Activity!</h2>
+}
+
