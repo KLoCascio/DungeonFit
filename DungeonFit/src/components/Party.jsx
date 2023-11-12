7bd4f407eff1b175a911249a27b47d058e804cb6
BWhittaker1415 <bwhittaker1415@gmail.com>
2023-11-12 15:35:21 -0500

    CSS party and invite, calendar party page

--- DungeonFit/src/components/Party.jsx
+++ DungeonFit/src/components/Party.jsx
@@ -1,84 +1,89 @@
-import { useEffect, useState } from "react"
-import { useParams, Link } from 'react-router-dom'
-import 'react-date-range/dist/styles.css'
-import 'react-date-range/dist/theme/default.css'
-import axios from 'axios'
-import { DateRangePicker } from "react-date-range"
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import Calendar from "react-calendar";
+// import 'react-date-range/dist/styles.css'
+// import 'react-date-range/dist/theme/default.css'
+import axios from "axios";
+import { DateRangePicker } from "react-date-range";
 
 export default function Party() {
+  const [date, setDate] = useState(new Date());
+  const [users, setUsers] = useState([]);
+  const [challengeName, setChallengeName] = useState("")
+  const [challengeDescription, setChallengeDescription] = useState("")
+  const [groupMembers, setGroupMembers] = useState([]);
 
-    const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const fetchAllUsers = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/user");
+        setUsers(response.data);
+      } catch (error) {
+        console.error("Error fetching members:", error);
+      }
+    };
 
-    useEffect(() => {
-        const fetchUsers = async () => {
-            try {
-                const response = await axios.get('http://localhost:3001/seed/user')
-                setUsers(response.data);
-            } catch (error) {
-                console.error('Error fetching members:', error);
-            }
-        };
+    fetchAllUsers();
+  }, []);
 
-        fetchUsers();
-    }, []);
 
-    const selectionRange = {
-        startDate: new Date(),
-        endDate: new Date(),
-        key: 'selection',
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const newMember = { id: Date.now()}
+    setGroupMembers((prevMembers) => [...prevMembers])
+  };
 
-    const handleSelect = (ranges) => {
-        console.log(ranges)
-    }
+  return (
+    <div className="Party">
+      {/* map function to show everyone added to your group? */}
+      <div className="members">
+        <h4>Members:</h4>
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
 
-    return (
-        <div className="Party">
-            {/* map function to show everyone added to your group? */}
-            <div className="members">
-                <h4>Members:</h4>
-                <ul>
-                    {users.map((user) => (
-                        <li key={user.id}>{user.name}</li>
-                    ))}
-                </ul>
+        <button className="add-members">
+          <Link to="/invite">Invite</Link>
+        </button>
+      </div>
+      <h3>Setup Party Challenge</h3>
+      <form className="challenge">
+        <label htmlFor="challenge-name">Pick a Name for Challenge:</label>
+        <br />
+        <input
+          type="text"
+          id="challenge"
+          className="challenge-name"
+          placeholder="Name of challenge..."
+          value={challengeName}
+          onChange={(e) => setChallengeName(e.target.value)}
+        />
+        <br />
 
-                <button className="add-members">
-                    <Link to="/invite">Invite</Link>
-                </button>
+        <label htmlFor="challenge-description">Challenge Description</label>
+        <br />
+        <input
+          type="text"
+          id="description"
+          className="challenge-description"
+          placeholder="description"
+          value={challengeDescription}
+          onChange={(e) => setChallengeDescription(e.target.value)}
+        />
 
-            </div>
-            <h3>Setup Party Challenge</h3>
-            <form className="challenge">
-                <label 
-                htmlFor="challenge-name">Pick a Name for Challenge:</label>
-                <br />
-                <input 
-                type="text" 
-                id="challenge"
-                className="challenge-name" 
-                placeholder="Name of challenge..."
-                value={''}
-                />
-                <br />
-
-                <label 
-                htmlFor="challenge-description">Challenge Description</label>
-                <br />
-                <input 
-                type="text" 
-                id="description"
-                className="challenge-description"
-                placeholder="description" 
-                value={''} 
-                />
-
-                {/* <label htmlFor="challenge-duration">Select a Duration:</label>
-                <DateRangePicker
-                    ranges={[selectionRange]}
-                    onChange={handleSelect}
-                /> */}
-            </form>
+        <div className="challenge-duration">
+          <h2 className="calendar-header">Select Challenge Duration:</h2>
+          <div className="calendar-container">
+            <Calendar onChange={setDate} value={date} />
+          </div>
+          <div className="selected-date-center">
+            Selected date: {date.toDateString()}
+          </div>
         </div>
-    )
+            <button type="submit">Submit Challenge!</button>
+      </form>
+    </div>
+  );
 }
