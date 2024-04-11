f723b708fc841f366dc060217b1a8a82a53e1685
BWhittaker1415 <bwhittaker1415@gmail.com>
2024-04-11 15:12:10 -0400

    adjusted all image and logo import paths

--- DungeonFit/src/components/Footer.jsx
+++ DungeonFit/src/components/Footer.jsx
@@ -1,14 +1,13 @@
-import { useEffect, useState } from 'react'
-import { useParams, Link } from 'react-router-dom'
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
 
-import axios from 'axios'
-import NavMobile from './NavMobile'
+import axios from "axios";
+import NavMobile from "./mobileComponents/NavMobile";
 
 export default function Footer() {
-    return (
-        <div className="Footer">
-           <NavMobile />
-        </div>
-    )
-
+  return (
+    <div className='Footer'>
+      <NavMobile />
+    </div>
+  );
 }
--- DungeonFit/src/components/Home.jsx
+++ DungeonFit/src/components/Home.jsx
@@ -1,23 +1,23 @@
-import CharacterSummary from './CharacterSummary'
+import CharacterSummary from "./characterPage/CharacterSummary";
 
 // FEEDS
-import AchievementFeed from '../feeds/AchievementFeed'
-import GoalsFeed from '../feeds/GoalsFeed'
-import NewsFeed from '../feeds/NewsFeed'
+import AchievementFeed from "../feeds/AchievementFeed";
+import GoalsFeed from "../feeds/GoalsFeed";
+import NewsFeed from "../feeds/NewsFeed";
 
 const Home = () => {
   return (
     <div>
-        <CharacterSummary />
-        <NewsFeed />
-        {/* MVP: Show User Achievements Completed
+      <CharacterSummary />
+      <NewsFeed />
+      {/* MVP: Show User Achievements Completed
         Stretch: Show friends Achieveents Completed */}
-        {/* <AchievementFeed /> */}
-        {/* MVP: Show User Quests
+      {/* <AchievementFeed /> */}
+      {/* MVP: Show User Quests
         SUPER Stretch: Close to complete Achievement */}
-        {/* <GoalsFeed /> */}
+      {/* <GoalsFeed /> */}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
